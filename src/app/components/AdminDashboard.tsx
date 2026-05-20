db0d343e5440af8d7c92aec32c525f22b8fb762a
woodieyeah <ewoodbry@gmail.com>
2026-05-20 16:33:27 +0700

    Update AdminDashboard.tsx
    changed copy in subject

--- src/app/components/AdminDashboard.tsx
+++ src/app/components/AdminDashboard.tsx
@@ -393,7 +393,7 @@ export function AdminDashboard({ data, onNavigateAdStudio }: { data?: any, onNav
                                     </div>`
       : "";
 
-    setSubject(`RightEdge: ${selectedTeam} model read for Round ${round}`);
+    setSubject(`RightEdge: ${selectedTeam} match play for ${round}`);
     setBody(`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" lang="en">
   <head>
